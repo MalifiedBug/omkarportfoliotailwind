17e64dc9c9ac9cd5a366206f59e8fe725538c2a5
omkar lolge <omkarlolge119.ol@gmail.com>
2022-11-27 17:34:57 +0530

    footer added

--- src/Footer.js
+++ src/Footer.js
@@ -3,14 +3,14 @@ export default function Footer() {
     <footer aria-label="Site Footer" class="bg-gray-100 dark:bg-gray-900">
       <div class="mx-auto max-w-5xl px-4 py-16 sm:px-6 lg:px-8">
         <p class="mx-auto mt-6 max-w-md text-center leading-relaxed text-gray-500 dark:text-gray-400">
-          <b>Building businesses together...</b>
+          <b class="text-white">Building businesses together...</b>
         </p>
 
         <nav aria-label="Footer Nav" class="mt-12">
           <ul class="flex flex-wrap justify-center gap-6 md:gap-8 lg:gap-12">
             <li>
               <a
-                class="text-gray-700 transition hover:text-gray-700/75 dark:text-white dark:hover:text-white/75"
+                class="text-white border-2 p-2 rounded-md text-gray-700 transition hover:text-gray-700/75 dark:text-white dark:hover:text-white/75"
                 href="https://www.linkedin.com/in/omkar-lolge-a34b5024b/"
                 target="_blank"
                 rel="noopener noreferrer"
@@ -21,7 +21,7 @@ export default function Footer() {
 
             <li>
               <a
-                class="text-gray-700 transition hover:text-gray-700/75 dark:text-white dark:hover:text-white/75"
+                class="text-white border-2 p-2 rounded-md text-gray-700 transition hover:text-gray-700/75 dark:text-white dark:hover:text-white/75"
                 href="https://medium.com/@malifiedbug"
                 target="_blank"
                 rel="noopener noreferrer"
@@ -32,7 +32,7 @@ export default function Footer() {
 
             <li>
               <a
-                class="text-gray-700 transition hover:text-gray-700/75 dark:text-white dark:hover:text-white/75"
+                class="text-white border-2 p-2 rounded-md text-gray-700 transition hover:text-gray-700/75 dark:text-white dark:hover:text-white/75"
                 href="https://github.com/MalifiedBug"
                 target="_blank"
                 rel="noopener noreferrer"
@@ -43,7 +43,7 @@ export default function Footer() {
 
             <li>
               <a
-                class="text-gray-700 transition hover:text-gray-700/75 dark:text-white dark:hover:text-white/75"
+                class="text-white border-2 p-2 rounded-md text-gray-700 transition hover:text-gray-700/75 dark:text-white dark:hover:text-white/75"
                 href="tel:+919321089065"
                 target="_blank"
                 rel="noopener noreferrer"
@@ -54,7 +54,7 @@ export default function Footer() {
 
             <li>
               <a
-                class="text-gray-700 transition hover:text-gray-700/75 dark:text-white dark:hover:text-white/75"
+                class="text-white border-2 p-2 rounded-md transition hover:text-gray-700/75 dark:text-white dark:hover:text-white/75"
                 href="https://twitter.com/MalifiedB"
                 target="_blank"
                 rel="noopener noreferrer"
